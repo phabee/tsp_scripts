711d24033793f4650ff6e19f95a60ba8f3f59ae7
Fabian Leuthold <fabian.leuthold@gmail.com>
2020-12-07 14:32:14 +0100

    improved search params

--- ortools_tsp_from_tsplib.py
+++ ortools_tsp_from_tsplib.py
@@ -1,8 +1,7 @@
-# Source: https://developers.google.com/optimization/routing/tsp
-# Adapted to read TSP instance from tsplib format and build dima
-# using scipy
+# https://developers.google.com/optimization/routing/tsp
 
 """Simple travelling salesman problem between cities."""
+# read instance from tsplib format
 
 from __future__ import print_function
 from ortools.constraint_solver import routing_enums_pb2
@@ -17,21 +16,6 @@ def create_data_model(dima):
     """Stores the data for the problem."""
     data = {}
     data['distance_matrix'] = dima
-    # data['distance_matrix'] = [
-    #     [0, 2451, 713, 1018, 1631, 1374, 2408, 213, 2571, 875, 1420, 2145, 1972],
-    #     [2451, 0, 1745, 1524, 831, 1240, 959, 2596, 403, 1589, 1374, 357, 579],
-    #     [713, 1745, 0, 355, 920, 803, 1737, 851, 1858, 262, 940, 1453, 1260],
-    #     [1018, 1524, 355, 0, 700, 862, 1395, 1123, 1584, 466, 1056, 1280, 987],
-    #     [1631, 831, 920, 700, 0, 663, 1021, 1769, 949, 796, 879, 586, 371],
-    #     [1374, 1240, 803, 862, 663, 0, 1681, 1551, 1765, 547, 225, 887, 999],
-    #     [2408, 959, 1737, 1395, 1021, 1681, 0, 2493, 678, 1724, 1891, 1114, 701],
-    #     [213, 2596, 851, 1123, 1769, 1551, 2493, 0, 2699, 1038, 1605, 2300, 2099],
-    #     [2571, 403, 1858, 1584, 949, 1765, 678, 2699, 0, 1744, 1645, 653, 600],
-    #     [875, 1589, 262, 466, 796, 547, 1724, 1038, 1744, 0, 679, 1272, 1162],
-    #     [1420, 1374, 940, 1056, 879, 225, 1891, 1605, 1645, 679, 0, 1017, 1200],
-    #     [2145, 357, 1453, 1280, 586, 887, 1114, 2300, 653, 1272, 1017, 0, 504],
-    #     [1972, 579, 1260, 987, 371, 999, 701, 2099, 600, 1162, 1200, 504, 0],
-    # ]  # yapf: disable
     data['num_vehicles'] = 1
     data['depot'] = 0
     return data
@@ -87,8 +71,16 @@ def main():
 
     # Setting first solution heuristic.
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    # search_parameters.first_solution_strategy = (
+        # routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
     search_parameters.first_solution_strategy = (
-        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+        routing_enums_pb2.FirstSolutionStrategy.AUTOMATIC)
+
+    search_parameters.local_search_metaheuristic = (
+        routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH)
+
+    search_parameters.time_limit.seconds = 30
 
     # Solve the problem.
     solution = routing.SolveWithParameters(search_parameters)
@@ -96,7 +88,7 @@ def main():
     # Print solution on console.
     if solution:
         print_solution(manager, routing, solution)
-
+        print("Solution is ", round(solution.ObjectiveValue() / 7544 * 100 - 100), "% more expensive than optimum." )
 
 if __name__ == '__main__':
     main()
